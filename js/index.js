a32746e37fb67b6bec1c7a12f0a9f41ab86bfc3d
Daisy Kirui <daisykkirui@gmail.com>
2022-06-24 00:42:31 +0300

    Created and styled a bouquet input form

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let inputForm = document.getElementById("create-task-form");
+    inputForm.addEventListener("submit", createNovelTask);
+  });
+
+  // create-task-form == flowerType
+  // new-task-description ==enterFlowerType
+
+  // create-task-form == flowerNumber
+  //new-task-description == enterFlowerNumber
