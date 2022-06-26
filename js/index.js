f70b228d3455261f91cc7dcfb40e0221f67d74fb
Daisy Kirui <daisykkirui@gmail.com>
2022-06-26 20:18:23 +0300

    Added an event listener

--- js/index.js
+++ js/index.js
@@ -1,10 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let inputForm = document.getElementById("create-task-form");
-    inputForm.addEventListener("submit", createNovelTask);
+  const myTitle = document.getElementById("myTitle");
+  console.log(myTitle.textContent);
+});
+
+const createCustomBouquet = function (event) {
+  event.preventDefault();
+  let typeOfFlower = document.getElementById("flowerType");
+  let numberOfFlower = document.getElementById("flowerNumber");
+  customBouquet.innerText = typeOfFlower.value && numberOfFlower.value;
+  appendNumberOfFlower(numberOfFlower);
+  event.target.reset();
+}
+console.log(customBouquet);
+
+
+
+/*
+document.addEventListener("DOMContentLoaded", () => {
+    let inputFlowerType = document.getElementById("flowerType");
+    total.addEventListener("submit", createCustomBouquet);
   });
 
-  // create-task-form == flowerType
-  // new-task-description ==enterFlowerType
-
-  // create-task-form == flowerNumber
-  //new-task-description == enterFlowerNumber
+  const createCustomBouquet = function (event) {
+    event.preventDefault();
+    let newTaskList = document.getElementById("bouquet");
+    let bouquetFlowers = document.createElement("li");
+    let customBouquetType = document.getElementById("enterFlowerType");
+    customBouquet.innerText = customBouquetType.value && customBouquetNumber;
+    console.log(customBouquet);
+    appendcustomBouquet(customBouquet);
+    event.target.reset();
+  };
+  const erase = (event) => {
+    event.target.ul.remove();
+  };
+  
+  const appendcustomBouquet = (bouquet) => {
+    document.getElementById("bouquet").appendChild(bouquet);
+  };
+*/
